e83c6507977faf35b673f3bae641df61df38025d
Ploven <plush337@gmail.com>
2024-09-05 11:27:53 +0300

    static base

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "docs",
+  assetPrefix:
+    "https://raw.githubusercontent.com/plaunezkiy/eutic-website/main/docs",
 };
 
 export default nextConfig;
